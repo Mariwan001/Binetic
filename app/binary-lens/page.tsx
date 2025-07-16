d7db9b540fb49061a8c80e162a84e81976565cbb
Mariwan <sehadmarewan@gmail.com>
2025-07-16 23:34:15 +0300

    Render Binary component at /binary-lens route (replace placeholder)

--- app/binary-lens/page.tsx
+++ app/binary-lens/page.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
+import Binary from '../../Binary/Binary';
 
-export default function BinaryLensPage() {
-  return (
-    <main className="w-full min-h-screen flex flex-col items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold mb-4 text-cyan-400">Binary Lens</h1>
-      <p className="text-lg text-white/70">This is the Binary Lens page. Content coming soon!</p>
-    </main>
-  );
+export default function Page() {
+  return <Binary />;
 } 
