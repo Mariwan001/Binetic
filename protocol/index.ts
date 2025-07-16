b47b09019fda45df4b2bdfd8c23adb5f0214fad8
Mariwan <sehadmarewan@gmail.com>
2025-07-16 21:30:43 +0300

    Add protocol/index.ts file with required exports

---
+++ protocol/index.ts
@@ -0,0 +1,22 @@
+export const Definition = `Protocol Flow is where Binetic exposes how machines speak translating raw data traffic into visible, traceable streams of logic in motion.`
+
+
+export const DescText1 = `This isn’t just about packets it's about transforming invisible exchanges into motion, abstraction into observable protocol logic.`
+
+
+export const DescText2 = `Data flows like a current. Protocol Flow maps every fork, dam, stream, and whirlpool showing how information navigates systems, adapts to errors, and reshapes its course in real time.`
+
+
+export const MainTextQuestion = 'What is Protocol Flow?'
+
+
+export const Questions = [
+    {
+        Q1: ` What It Does?`,
+        Ans1: 'Protocol Flow maps out the invisible highways of digital interaction—from internal process syncs to global API requests. It decodes how devices, programs, and networks agree on how, when, and what to send.' 
+    },
+    {
+        Q2: `Why It Matters?`,
+        Ans2: 'Modern systems fail when protocols break. Binetic’s Protocol Flow gives developers, engineers, and thinkers a lens to detect weakness, optimize paths, and understand digital negotiation at its purest level.' 
+    },
+]
